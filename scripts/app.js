33e37edd6a1d4ad09cfa5fd7e549ddd3bc4b1030
Cliff Conolly <cliffconolly@protonmail.com>
2019-12-30 20:06:08 +0000

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+
+
+// var controller = ScrollMagic.Controller()
+//
+// var scene = new ScrollMagic.Scene({
+//   triggerElement: '.red-cube'
+//
+// })
+//
+//   .setClassToggle('.red-cube', 'show')
+//   .addTo(controller)
+//
+//
+// console.log('Hello')
+
+
+
+
+// init controller
+// var controller = new ScrollMagic.Controller()
+// // build scene
+// new ScrollMagic.Scene({
+//   triggerElement: '#trigger1',
+//   triggerHook: 0.9, // show, when scrolled 10% into view
+//   duration: '80%', // hide 10% before exiting view (80% + 10% from bottom)
+//   offset: 50 // move trigger to center of element
+// })
+//   .setClassToggle('#reveal1', 'visible') // add class to reveal
+//   .addIndicators() // add indicators (requires plugin)
+//   .addTo(controller)
